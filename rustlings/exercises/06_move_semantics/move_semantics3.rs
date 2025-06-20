ac7ba1a21c778be34933b111d35dbda70891cf51
Devansh Tyagi <devanshtyagi0607@gmail.com>
2025-06-20 23:45:17 +0530

    32/94

--- rustlings/exercises/06_move_semantics/move_semantics3.rs
+++ rustlings/exercises/06_move_semantics/move_semantics3.rs
@@ -1,12 +1,13 @@
 // TODO: Fix the compiler error in the function without adding any new line.
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+fn fill_vec(mut vec: Vec<i32>) -> Vec<i32> {
     vec.push(88);
 
     vec
 }
 
 fn main() {
-    // You can optionally experiment here.
+    let mut vec: Vec<i32> = Vec::new();
+    vec.push(20);
 }
 
 #[cfg(test)]
